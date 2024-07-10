ee10e8070a2dd69183b98105e6027cae32bb17cb
MarieRmsy <ramssamy.marie@gmail.com>
2024-07-10 14:01:25 +0200

    push front marie

---
+++ frontmarie/app/src/pages/inscription.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import '../assets/style.css';
+
+const InscriptionPage = () => {
+   
+      return (
+        <div className="blocConnexion">
+          <h1>Inscription</h1>
+          <div className="blocConnexion">
+            ee
+          </div>
+        </div>
+      );
+    };
+    
+    export default InscriptionPage;
+    
